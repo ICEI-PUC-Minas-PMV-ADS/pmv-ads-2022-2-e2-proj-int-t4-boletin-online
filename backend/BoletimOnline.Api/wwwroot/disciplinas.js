c962d8268353fbcfd81b1df360fd8d6e8a0fcea5
fcostar <fisicocosta@gmail.com>
2022-11-26 16:57:00 -0300

    adicionando end point disciplinas

---
+++ backend/BoletimOnline.Api/wwwroot/disciplinas.js
@@ -0,0 +1,31 @@
+function getDisciplinas() {
+    fetch('api/Disciplinas')
+        .then(response => response.json())
+        .then(data => displayDisciplinas(data))
+        .catch(error => console.error('Erro ao buscar disciplinas ! ', error));
+}
+
+function displayDisciplinas(data) {
+    let list = document.getElementById("listaDeDisciplina");
+    let slice = Math.ceil(data.length  / 4)
+
+        for (let i = 0; i < data.length; i = i + slice) {
+            const newLi = data.slice(i, i + slice);
+            let newUl = document.createElement("ul");
+            newUl.setAttribute("class", "primeirafileira")
+            newLi.map(disciplina => {
+                let li = document.createElement("button");
+                newUl.setAttribute("class", "botao1-1")
+                li.onclick = () => goListarProfessores(disciplina.id, disciplina.nome);
+                li.textContent = `${disciplina.nome}`;
+                newUl.appendChild(li);
+            })
+            
+            list.appendChild(newUl);
+        }
+        
+}
+
+function goListarProfessores(id, nome) {
+    window.location.href = `${window.location.origin}/ListarProfessores.html?disciplinaId=${id}&disciplinaNome=${nome}`;
+}
